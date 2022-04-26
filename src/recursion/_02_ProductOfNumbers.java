17596a09bfefb06ddaa58902f1b7ef389d194697
ReginaBik <reginabiktimirova1@gmail.com>
2022-04-25 19:03:34 -0500

    added recursion

---
+++ src/recursion/_02_ProductOfNumbers.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class _02_ProductOfNumbers {
+    public static int recursiveProduct(int n){
+        if(n > 0) return n * recursiveProduct(n - 1);
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(recursiveProduct(5));
+    }
+
+
+}
