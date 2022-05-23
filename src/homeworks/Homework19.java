24ab3aae32cc7c1167c23600996cee567fb45512
ReginaBik <reginabiktimirova1@gmail.com>
2022-05-23 00:09:08 -0500

    added task 2 and 3

--- src/homeworks/Homework19.java
+++ src/homeworks/Homework19.java
@@ -6,9 +6,10 @@ public class Homework19 {
 -Create a method called noDigit()
 -This method will take one String argument and it will
 return a new String with all digits removed from the
-”        _______Task 1.0________*/
+”        _______Task 1________*/
 
     public static String noDigit(String str){
+
         return str.replaceAll("[0-9]", "");
     }
     /*_______Task 1.1________ */
@@ -21,11 +22,45 @@ return a new String with all digits removed from the
         return noDigString;
     }*/
 
-    public static void main(String[] args) {
-        System.out.println(Homework19.noDigit("hdhhd222"));
+   /* -Create a method called noVowel()
+-This method will take one String argument and it will
+return a new String with all vowels removed from the
+    original String
+    _______Task 2________ */
 
-
+    public static String noVowel(String str){
+        return str.toLowerCase().replaceAll("[aeouiy]", "");
     }
 
+    /* -Create a method called sumOfDigits()
+-This method will take one String argument and it will return an int sum
+    of all digits from the original String.
+      _______Task 2________ */
+
+    public static int sumOfDigits(String str){
+        int sum = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(Character.isDigit(str.charAt(i))) sum += Integer.parseInt("" + str.charAt(i));
+        }
+        return sum;
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Task1\n");
+        System.out.println(Homework19.noDigit("hdhhd222"));
+
+        System.out.println("\nTask2\n");
+        System.out.println(Homework19.noVowel("afaniusetayusn"));
+
+        System.out.println("\nTask3\n");
+        System.out.println(Homework19.sumOfDigits("h2mk2m4"));
+    }
 
 }
