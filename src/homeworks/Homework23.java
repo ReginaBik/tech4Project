fcd750b7000c18bafeecb9ae8d6a5a4bc0aaa8a0
ReginaBik <reginabiktimirova1@gmail.com>
2022-06-23 19:29:14 -0500

    created Homewrok 23 class

---
+++ src/homeworks/Homework23.java
@@ -0,0 +1,32 @@
+package homeworks;
+
+public class Homework23 {
+
+    /*Task 1
+    * Requirement:
+    Write a method called as parseData() which takes a
+    String has some keys in {} and values after between }{
+    and returns a collection that has all the keys and values
+    as entries.
+    NOTE: The keys should be sorted*/
+
+    /*Task 2
+    * Requirement:
+    Write a method called as calculateTotalPrice1() which takes
+    a Map of some shopping items with their amounts and
+    calculates the total prices as double. Item prices are given
+    below
+    Apple = $2.00
+    Orange = $3.29
+    Mango = $4.99
+    Pineapple = $5.25*/
+
+    /*Task 3
+    Requirement:
+    Write a method calculateTotalPrice2() which takes a Map of some
+    shopping items with their amounts and calculates the total prices as
+    double. Item prices are given below
+    Apple = $2.00
+    Orange = $3.29
+    Mango = $4.99*/
+}
