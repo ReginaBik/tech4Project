edee344a02d9c4d4af3c784ee6cc5dd7afc5b9e1
ReginaBik <reginabiktimirova1@gmail.com>
2022-06-29 23:14:23 -0500

    added Reversed String Task

--- src/javaPractice/Practice.java
+++ src/javaPractice/Practice.java
@@ -2,16 +2,32 @@ package javaPractice;
 
 public class Practice {
 
+    //Reverse String different ways
+
+    //StringBuilder (mutable)
+
     public static void reverseString(String str){
         StringBuilder reversed = new StringBuilder();
         for (int i = str.length()-1; i >= 0; i--){
             reversed.append(str.charAt(i));
         }
-        System.out.println(reversed.toString());
+        System.out.println(reversed);
+    }
+
+    //Regular String
+    public static String reverseString2(String str){
+        String reversed = "";
+        for(int i = str.length()-1; i >=0; i--){
+            reversed += str.charAt(i) + "";
+        }
+        return reversed;
     }
 
     public static void main(String[] args) {
         reverseString("Marzipan");
+        System.out.println(reverseString2("Switzerland is gorgeous"));
 
     }
+
+
 }
