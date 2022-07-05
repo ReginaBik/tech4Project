ef92b4b7b38555d63a5203afe102bc3e56e7b21d
ReginaBik <reginabiktimirova1@gmail.com>
2022-07-05 16:51:20 -0500

    added commit

--- src/homeworks/Mock.java
+++ src/homeworks/Mock.java
@@ -2,7 +2,6 @@ package homeworks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.TreeSet;
 
 public class Mock {
@@ -22,6 +21,27 @@ public class Mock {
         TreeSet<Integer> set = new TreeSet<>(Arrays.asList(a, b, c));
         ArrayList<Integer> list = new ArrayList<>(set);
         return list.get(1);
+    }
+
+        /*
+    Write a method that takes 3 int arguments and returns the middle number
+    NOTE: 2 or 3 numbers being same is okay
+    TEST DATA:
+    3, 4, 5
+    EXPECTED OUTPUT:
+    4
+     */
+
+    public static int findMiddleOf3Numbers(int a, int b, int c){
+        if(a == b || b == c) return b;
+        else if(a == c) return a;
+
+        int greatest = Math.max(a, Math.max(b, c));
+        int smallest = Math.min(a, Math.min(b, c));
+
+        if(a != greatest && a != smallest) return a;
+        else if(b != greatest && b != smallest) return b;
+        return c;
     }
 
 
